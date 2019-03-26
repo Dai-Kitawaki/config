2599001db66071dd4c87d3ab4a9b97fb91cfa8e4
Dai-Kitawaki <48936097+Dai-Kitawaki@users.noreply.github.com>
2019-03-26 12:43:38 +0900

    Modify route

--- test.rb
+++ test.rb
@@ -3,21 +3,9 @@ tdc-fw201
 ------------------
 #-----------------------------Modify the link-monitor
 config router static
-    edit 1
-        set dst 101.102.253.5 255.255.255.255
-        set device "wan1"
-        set dynamic-gateway enable
-    next
-    edit 2
-        set distance 250
-        set device "tdc_vpn1"
-    next
-    edit 3
-        set dst 39.110.217.37 255.255.255.255
-        set device "wan1"
-        set dynamic-gateway enable
-    next
     edit 4
-        set device "tdc_vpn2"
+        set distance 251
     next
 end
+
+# chk route
